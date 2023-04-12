22c2ede5fe90dc58221db3328f4ae591fe4d87eb
Luferov Victor <lyferov@yandex.ru>
2023-04-13 00:02:41 +0300

    fix: Некоторые правки

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -9,9 +9,9 @@ useHead({
 </script>
 <template>
   <div class="grid text-center place-items-center mt-5">
-    <h1>{{ $t('index.header1') }}</h1>
-    <h2>{{ $t('index.header2') }}</h2>
-    <img src="/images/truck.svg" alt="" />
+    <h1>{{ $t('index.header') }}</h1>
+    <h2>{{ $t('index.title') }}</h2>
+    <v-img src="/images/truck.svg" class="w-1/3" alt="" />
     <p class="text-base">{{ $t('index.info') }}</p>
   </div>
 </template>
