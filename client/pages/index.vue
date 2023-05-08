8be861bf4f80290d19634346719af7b1efd3edb6
Luferov Victor <lyferov@yandex.ru>
2023-05-08 23:24:06 +0300

    feat: change price view in search products

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,78 +1,26 @@
 <script setup lang="ts">
-import { useFilters, useHead, useI18n } from '#imports'
-import productsQuery from '~/graphql/products/queries/products.graphql'
-import { ProductsQuery, ProductsQueryVariables } from '~/types/graphql'
-import { useOffsetPagination } from '~/composables/pagination'
+import { useHead, useI18n, useLocalePath } from '#imports'
 
+const localePath = useLocalePath()
 const { t } = useI18n()
-const { date } = useFilters()
 useHead({
   title: t('title'),
 })
 
-const search = ref<string>('')
-
-const {
-  data: products,
-  loading,
-  pagination,
-} = useQueryRelay<ProductsQuery, ProductsQueryVariables>(
-  {
-    document: productsQuery,
-    variables: () => ({
-      search: search.value,
-    }),
-    options: {
-      debounce: 250,
-    },
-  },
-  { pagination: useOffsetPagination({ pageSize: 10 }) },
-)
-const headers = [
-  { title: '#', key: 'id', sortable: false },
-  { title: 'Артикул', key: 'vendorCode', sortable: false },
-  { title: 'Название', key: 'name', sortable: false },
-  { title: 'Производитель', key: 'manufacturer', sortable: false },
-  { title: 'Цены', key: 'prices', sortable: false },
+const cards: Record<string, string>[] = [
+  { key: 'products', title: 'Поиск продуктов', to: 'products', icon: 'mdi-train-car-autorack' },
+  { key: 'orders', title: 'Мои заказы', to: 'orders', icon: 'mdi-basket' },
 ]
 </script>
 <template>
   <v-container>
     <v-row>
-      <v-col>
-        <v-text-field
-          v-model="search"
-          :label="$t('index.vendorCode')"
-          :loading="loading"
-          append-inner-icon="mdi-magnify"
-          density="compact"
-          hide-details
-          single-line
-        />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-card>
-          <v-data-table-server
-            v-model:items-per-page="pagination.pageSize.value"
-            v-model:page="pagination.page.value"
-            :headers="headers"
-            :loading="loading"
-            :items="products"
-            :items-length="pagination.totalCount.value"
-          >
-            <template #[`item.prices`]="{ item }">
-              <v-list density="compact">
-                <v-list-item
-                  v-for="price in item.raw.prices"
-                  :key="price.id"
-                  :title="`${price.price} (${price.duration})`"
-                  :subtitle="`${price.supplierName} - ${date(price.createdAt)}`"
-                />
-              </v-list>
-            </template>
-          </v-data-table-server>
+      <v-col v-for="card in cards" :key="card.key" :cols="12 / cards.length">
+        <v-card :to="localePath({ name: card.to })">
+          <v-card-text class="text-center">
+            <v-icon :icon="card.icon" size="100px" />
+          </v-card-text>
+          <v-card-title>{{ card.title }}</v-card-title>
         </v-card>
       </v-col>
     </v-row>
